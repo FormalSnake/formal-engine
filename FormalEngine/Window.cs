1d5436288f5308da54962dea9c565f778c83d387
superlopez-real <kakskemail@gmail.com>
2023-02-28 12:17:24 +0000

    Spinning cube

--- FormalEngine/Window.cs
+++ FormalEngine/Window.cs
@@ -5,6 +5,7 @@ using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Windowing.Desktop;
+using System.Diagnostics;
 
 namespace FormalEngine
 {
@@ -12,6 +13,10 @@ namespace FormalEngine
     // with several point lights
     public class Window : GameWindow
     {
+        private float _lastTimestamp = Stopwatch.GetTimestamp();
+        private float _freq = Stopwatch.Frequency;
+
+        private float _angle;
         private readonly float[] _vertices =
         {
             // Positions          Normals              Texture coords
@@ -60,8 +65,8 @@ namespace FormalEngine
 
         private readonly Vector3[] _cubePositions =
         {
-            new Vector3(0.0f, 0.0f, 0.0f),
-            new Vector3(2.0f, 5.0f, -15.0f),
+            new Vector3(0.0f, 0.0f, 0.0f)
+            /*new Vector3(2.0f, 5.0f, -15.0f),
             new Vector3(-1.5f, -2.2f, -2.5f),
             new Vector3(-3.8f, -2.0f, -12.3f),
             new Vector3(2.4f, -0.4f, -3.5f),
@@ -69,7 +74,7 @@ namespace FormalEngine
             new Vector3(1.3f, -2.0f, -2.5f),
             new Vector3(1.5f, 2.0f, -2.5f),
             new Vector3(1.5f, 0.2f, -1.5f),
-            new Vector3(-1.3f, 1.0f, -1.5f)
+            new Vector3(-1.3f, 1.0f, -1.5f)*/
         };
 
         // We need the point lights' positions to draw the lamps and to get light the materials properly
@@ -147,7 +152,7 @@ namespace FormalEngine
                 GL.VertexAttribPointer(positionLocation, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
             }
 
-            _diffuseMap = Texture.LoadFromFile("Resources/container2.png");
+            _diffuseMap = Texture.LoadFromFile("Resources/formalsnake.png");
             _specularMap = Texture.LoadFromFile("Resources/container2_specular.png");
 
             _camera = new Camera(Vector3.UnitZ * 3, Size.X / (float)Size.Y);
@@ -213,11 +218,16 @@ namespace FormalEngine
             _lightingShader.SetFloat("spotLight.cutOff", MathF.Cos(MathHelper.DegreesToRadians(12.5f)));
             _lightingShader.SetFloat("spotLight.outerCutOff", MathF.Cos(MathHelper.DegreesToRadians(17.5f)));
 
+            
+
             for (int i = 0; i < _cubePositions.Length; i++)
             {
                 Matrix4 model = Matrix4.CreateTranslation(_cubePositions[i]);
                 float angle = 20.0f * i;
-                model = model * Matrix4.CreateFromAxisAngle(new Vector3(1.0f, 0.3f, 0.5f), angle);
+                var timeStamp = Stopwatch.GetTimestamp();
+                _angle += (float)((timeStamp - _lastTimestamp) / (double)_freq);
+                _lastTimestamp = timeStamp;
+                model = model * Matrix4.CreateFromAxisAngle(new Vector3(1.0f, 0.3f, 0.5f), _angle);
                 _lightingShader.SetMatrix4("model", model);
 
                 GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
